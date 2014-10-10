61567432cdc634aaac54fef87df53dcac73db81f
Daniel Eissing <daniel.eissing@sinnerschrader.com>
2014-10-10 14:25:44 +0200

    Adds option for html escaping. Updates package version.

--- tasks/markup-markdown.js
+++ tasks/markup-markdown.js
@@ -1,7 +1,27 @@
 /* jshint node:true */
 var _ = require('lodash');
+var htmlEscape = require('html-escape');
 
 var MarkupMarkdown = require('node-markup-markdown');
+
+function processJSON (contents, options) {
+
+  function recurseProcessJSON (contents) {
+    _.each(contents, function (value, key) {
+      if (_.isObject(value)) {
+        contents[key] = recurseProcessJSON(value);
+      }
+      else {
+        if (options.htmlSafe) {
+          value = htmlEscape(value);
+        }
+        contents[key] = MarkupMarkdown.parse(, options);
+      }
+    });
+    return contents;
+  }
+  return recurseProcessJSON(contents);
+}
 
 module.exports = function (grunt) {
   'use strict';
@@ -18,10 +38,18 @@ module.exports = function (grunt) {
 
           grunt.log.writeln('Processing ' + file);
 
-          var text = grunt.file.readFile(file.src);
-          var processed = MarkupMarkdown.parse(text, options);
+          var processed;
 
-          grunt.writeFile(fileDescription.dest, processed);
+          if (/\.json$/.test(file)) {
+            var contents = grunt.file.readJSON(file);
+            processed = JSON.stringify(processJSON(contents, options));
+          }
+          else {
+            var text = grunt.file.read(file);
+            processed = MarkupMarkdown.parse(text, options);
+          }
+
+          grunt.file.write(fileDescription.dest, processed);
         });
       });
     }
